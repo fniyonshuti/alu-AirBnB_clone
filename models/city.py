de0b4c53a4cc46faa6613373b4eb3bf89ccbf646
Irakoze Hortance <hortanceirakoze@gmail.com>
2024-09-07 18:30:43 +0200

    city file

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for City class
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    A class representing a city.
+    
+    """
+    state_id = ""
+    name = ""
